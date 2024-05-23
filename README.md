355d1f4383f94819ef5efe726abe7df4b27bab49
Admin <shapovals1991@gmail.com>
2024-05-23 17:09:01 +0300

    finish task

---
+++ README.md
@@ -0,0 +1,1 @@
+# **Application for sorting dates by letter**
--- src/main/java/DateSorter.java
+++ src/main/java/DateSorter.java
@@ -1,18 +1,16 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class DateSorter {
 	private final String SORTING_LETTER = "r";
 
 	public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+		if (unsortedDates == null || unsortedDates.isEmpty()) return Collections.emptyList();
+
 		List<LocalDate> withSortingLetter = unsortedDates.stream()
 				.filter(this::existsLetterInMonth)
 				.sorted(Comparator.naturalOrder())
 				.toList();
-
 		List<LocalDate> withoutSortingLetter = unsortedDates.stream()
 				.filter(date -> !existsLetterInMonth(date))
 				.sorted(Comparator.reverseOrder())
